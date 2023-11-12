3717f1fab0620164e689839dc6d302768f046739
Rafael Nóbrega <rafaelnobregadelima2809@gmail.com>
2023-11-12 10:43:34 -0400

    Projeto do chat

---
+++ src/backend/server.js
@@ -0,0 +1,33 @@
+const http = require ("http")
+const { Server } = require("socket.io")
+
+const httpServer = http.createServer();
+
+const PORT = process.env.PORT || 3001
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
+    },
+});
+
+io.on("connection", async (socket) => {
+    console.log("Usuário conectado: ", socket.id);
+    socket.on("join_room", (userName)=>{
+        console.log(`Username: ${userName} - Socket.id: ${socket.id}`);
+    });
+
+    socket.on("send-message", (msg)=>{
+        console.log(msg, "MSG");
+        io.emit("receive-msg", msg);
+    });
+
+});
+
+
+httpServer.listen(PORT, ()=>{
+    console.log(`Socket.io server esta rodando na porta ${PORT}`);
+})
